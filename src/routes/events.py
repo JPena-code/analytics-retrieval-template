25da418d36a28d14b58f9cbea932ba0673b8d41f
Julio Quintero <jequinterope@unal.edu.co>
2025-06-07 09:35:14 -0500

    Add POST layout method for event creation

--- src/routes/events.py
+++ src/routes/events.py
@@ -2,7 +2,7 @@ from ipaddress import IPv4Address
 
 from fastapi import APIRouter, Request
 
-from ..schemas import EventCreate, Response
+from ..schemas import EventCreate, EventSchema, Response
 
 router = APIRouter(tags=["events"])
 
@@ -11,23 +11,30 @@ router = APIRouter(tags=["events"])
     "/",
     response_model_by_alias=True,
     response_model_exclude_none=True,
-    response_model=Response[EventCreate],
+    response_model=Response[EventSchema],
 )
-async def events(req: Request):
-    return Response[EventCreate](
+def read_events(req: Request):
+    return Response[EventSchema](
         data=[
-            EventCreate(
-                path="/dummy/path/",
+            EventSchema(
+                id=1,
+                path="/dummy/path",
                 agent="dummy-agent",
-                ip_address=IPv4Address("172.0.0.1"),
+                ip_address=IPv4Address("127.0.0.1"),
                 session_id="dummy-session",
             ),
-            EventCreate(
-                path="/dummy/path/",
-                agent="dummy-agent",
+            EventSchema(
+                id=2,
+                path="/dummy/path/2",
+                agent="dummy-agent-2",
                 ip_address=IPv4Address("172.0.0.1"),
-                session_id="dummy-session",
+                session_id="dummy-session-2",
             ),
         ],
         req=req,
     )
+
+
+@router.post("/", response_model=Response[EventSchema], response_model_by_alias=True)
+def create_event(req: Request, payload: EventCreate):
+    return Response[EventSchema](**{"req": req, "data": EventSchema(**payload.model_dump(), id=1)})
