a80b5eec4935d70b7126b74524d1dbc0fad831d9
Julio Quintero <jequinterope@unal.edu.co>
2025-09-20 18:01:40 -0500

    Refactor: Enhance database initialization in db.py; add get_session to module exports

--- src/entrypoints/__init__.py
+++ src/entrypoints/__init__.py
@@ -2,9 +2,9 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from .db import init_db
+from .db import get_session, init_db
 
-__all__ = ["init_app"]
+__all__ = ["init_app", "get_session"]
 
 
 @asynccontextmanager
--- src/entrypoints/db.py
+++ src/entrypoints/db.py
@@ -1,29 +1,39 @@
-from sqlmodel import Session
+from sqlalchemy.schema import CreateSchema
+from sqlmodel import SQLModel, Session
 
+from .. import models
 from ..db import activate_ext, create_engine, sync_hypertables
 from ..settings import Settings
 
 SETTINGS = Settings()  # type: ignore
 
-__engine = None
+__engine = create_engine(
+    SETTINGS.pg_dsn.encoded_string(),
+    echo=True,
+    echo_pool=SETTINGS.debug,
+    pool_size=100,
+    pool_recycle=3600,
+    pool_timeout=30,
+)
 
 
 def init_db():
     """Initialize the database connection and the related setup
     In a lifespan cycle for the application
     """
+    if __engine is None:
+        raise RuntimeError("Database engine is not initialized")
+    with __engine.begin() as conn:
+        try:
+            conn.execute(CreateSchema(models.SCHEMA, if_not_exists=True))
+            activate_ext(conn, "timescaledb")
+            activate_ext(conn, "uuid-ossp")
 
-    __engine = create_engine(
-        SETTINGS.pg_dsn.encoded_string(),
-        echo=True,
-        echo_pool=SETTINGS.debug,
-        pool_size=10,
-        pool_recycle=3600,
-        pool_timeout=30,
-    )
-    activate_ext(__engine, "timescaledb")
-    activate_ext(__engine, "uuid-ossp")
-    sync_hypertables(__engine)
+            SQLModel.metadata.create_all(conn, checkfirst=True)
+            sync_hypertables(conn)
+        except:
+            conn.rollback()
+            raise
 
 
 def get_session():
