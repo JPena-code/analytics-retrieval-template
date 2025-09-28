541e73093c643978f60cf4afeb0a4c5e481a2167
Julio Quintero <jequinterope@unal.edu.co>
2025-09-27 23:03:01 -0500

    Refactor: Enhance logger integration in init_app and init_db functions; improve error handling and logging details

--- src/entrypoints/__init__.py
+++ src/entrypoints/__init__.py
@@ -3,11 +3,14 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from .db import get_session, init_db
+from .logger import get_logger, init_loggers
 
 __all__ = ["init_app", "get_session"]
 
 
 @asynccontextmanager
 async def init_app(app: FastAPI):
-    init_db()
-    yield
+    init_loggers()
+    print(get_logger())
+    init_db(get_logger())
+    yield {"logger": get_logger()}
--- src/entrypoints/db.py
+++ src/entrypoints/db.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.schema import CreateSchema
 from sqlmodel import SQLModel, Session
 
@@ -5,19 +6,17 @@ from .. import models
 from ..db import activate_ext, create_engine, sync_hypertables
 from ..settings import Settings
 
-SETTINGS = Settings()  # type: ignore
-
 __engine = create_engine(
-    SETTINGS.pg_dsn.encoded_string(),
+    Settings.pg_dsn.encoded_string(),
     echo=True,
-    echo_pool=SETTINGS.debug,
+    echo_pool=Settings.debug,
     pool_size=100,
     pool_recycle=3600,
     pool_timeout=30,
 )
 
 
-def init_db():
+def init_db(logger):
     """Initialize the database connection and the related setup
     In a lifespan cycle for the application
     """
@@ -25,13 +24,26 @@ def init_db():
         raise RuntimeError("Database engine is not initialized")
     with __engine.begin() as conn:
         try:
+            logger.info("Creating base schema %s", models.SCHEMA)
             conn.execute(CreateSchema(models.SCHEMA, if_not_exists=True))
+            logger.debug('Creating extensions in database ["timescaledb", "uuid-ossp"')
             activate_ext(conn, "timescaledb")
             activate_ext(conn, "uuid-ossp")
 
+            logger.info("Creating/Updating table models")
             SQLModel.metadata.create_all(conn, checkfirst=True)
-            sync_hypertables(conn)
+            sync_hypertables(logger, conn)
+        except SQLAlchemyError as e_sql:
+            logger.error(
+                "SQL Error encounter init_db (%s) (%d) (%s)",
+                type(e_sql).__name__,
+                e_sql.code,
+                e_sql._message(),
+            )
+            conn.rollback()
+            raise e_sql
         except:
+            logger.exception("Unexpected error encounter init_db")
             conn.rollback()
             raise
 
