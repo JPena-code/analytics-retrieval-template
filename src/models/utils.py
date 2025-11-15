f9fc8cfff9b2c655aa82fcd37e538a51c2021f78
Julio Quintero <jequinterope@unal.edu.co>
2025-11-14 21:09:39 -0500

    ADD: Implement is_numeric function to check if a field is numeric in SQLModel

---
+++ src/models/utils.py
@@ -0,0 +1,11 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sqlmodel import SQLModel
+
+
+def is_numeric(model: "type[SQLModel]", field_name: str):
+    field_info = model.model_fields.get(field_name)
+    if field_info is not None:
+        return field_info.annotation in {int, float}
+    return False
